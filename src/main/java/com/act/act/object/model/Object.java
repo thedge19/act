7a9c20753cb6221bb8dd9af7346429b87594e6cf
thedge19 <paz80@yandex.ru>
2025-03-09 13:05:03 +0300

    feat/create project#2

--- src/main/java/com/act/act/object/model/Object.java
+++ src/main/java/com/act/act/object/model/Object.java
@@ -4,9 +4,17 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Table(name = "objects")
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
 public class Object {
     @Id
     @Column(name = "object_id")
